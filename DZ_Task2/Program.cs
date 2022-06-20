f77398835900e1ef3b575d74455b716229277a54
Safiia <sonymamedova@gmail.com>
2022-06-20 18:16:18 +0300

    add DZ_Task2

--- DZ_Task2/Program.cs
+++ DZ_Task2/Program.cs
@@ -8,6 +8,6 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 int max = num2; 
-if (num2 > num1); max = num2;
+if (num1 < num2); max = num2;
 Console.Write("max = ");
 Console.WriteLine(max);
